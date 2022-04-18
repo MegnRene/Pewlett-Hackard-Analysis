ba73e6a3399848241d8262cf0cdf18de91393b12
MegnRene <98067116+MegnRene@users.noreply.github.com>
2022-04-18 12:32:20 -0700

    updated schema

--- schema.sql
+++ schema.sql
@@ -24,7 +24,7 @@ from_date DATE NOT NULL,
 to_date DATE NOT NULL, 
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
-	PRIMARY KEY (emp_no, dept_no
+	PRIMARY KEY (emp_no, dept_no)
 );
 	
  CREATE TABLE salaries (
@@ -33,25 +33,34 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
  	from_date DATE NOT NULL,
  	to_date DATE NOT NULL, 
  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
- PRIMARY KEY (emp_no)
+ PRIMARY KEY (emp_no, salary)
  );
 	
-CREATE TABLE titles (
-	emp_no INT NOT NULL, 
-	salary INT NOT NULL, 
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL, 
-PRIMARY KEY (emp_no) 
+CREATE TABLE titles(
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no, title, from_date)
 );
 	
-CREATE TABLE dept_emp (
+ 
+ CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR NOT NULL, 
 	from_date DATE NOT NULL, 
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no), 
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no), 
-	PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no, dept_no, from_date)
 );
 	
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+DROP TABLE dept_emp CASCADE;
